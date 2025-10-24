fe1056b2e66fb5f6a1681e622becf673f8fe1d57
Carlos Samir Diaz Gaviria <109492335+elsammio@users.noreply.github.com>
2025-10-24 09:12:01 -0500

    ACreación proyecto Healthy Track

---
+++ LuisaRincon/HealthyTrack/Habito.h
@@ -0,0 +1,13 @@
+//
+// Created by samir.diaz on 24/10/2025.
+//
+
+#ifndef HEALTHYTRACK_HABITO_H
+#define HEALTHYTRACK_HABITO_H
+
+
+class Habito {
+};
+
+
+#endif //HEALTHYTRACK_HABITO_H
---
+++ LuisaRincon/HealthyTrack/Usuario.h
@@ -0,0 +1,13 @@
+//
+// Created by samir.diaz on 24/10/2025.
+//
+
+#ifndef HEALTHYTRACK_USUARIO_H
+#define HEALTHYTRACK_USUARIO_H
+
+
+class Usuario {
+};
+
+
+#endif //HEALTHYTRACK_USUARIO_H
